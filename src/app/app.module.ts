40f6ee51c5ee2a2d39cb2482f35e0056d33db487
Joshua Ondieki <oj@joshuaondieki.com>
2023-05-24 01:38:29 +0300

    Setup ionicons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { IonicModule } from '@ionic/angular';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderComponent,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
