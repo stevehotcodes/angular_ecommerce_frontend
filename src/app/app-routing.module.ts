bccd0f4ab036e8bcfd47670e1980f471b8d9bca3
stevehotcodes <stevehotcodes@gmail.com>
2023-05-24 20:29:00 +0300

    added the signup and the signin routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainpageComponent } from './components/mainpage/mainpage.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 const routes: Routes = [
-  {path: '', component:MainpageComponent}
+  {path: '', component:MainpageComponent},
+  {path:"signup", component:SignupComponent},
+  {path:"signin", component:SigninComponent}
+  
 ];
 
 @NgModule({
