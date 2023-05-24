2ec5481ada51d0ce074b39ea3b963b8dfa857562
stevehotcodes <stevehotcodes@gmail.com>
2023-05-24 20:24:40 +0300

    just checked on the routing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import {IonicModule} from '@ionic/angular'
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, IonicModule, ReactiveFormsModule],
+  imports: [CommonModule, IonicModule, ReactiveFormsModule,RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
