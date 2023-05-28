8bb6ffc97471f900e3f000d7a9de9d1b2b927061
stevehotcodes <stevehotcodes@gmail.com>
2023-05-28 17:41:23 +0300

    added the form builder property , textColor and errorMsg Font size styling property

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup,ReactiveFormsModule,FormControl, Validators } from '@angular/forms';
+import { FormGroup,ReactiveFormsModule,FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -10,14 +10,24 @@ import { FormGroup,ReactiveFormsModule,FormControl, Validators } from '@angular/
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-  form!:FormGroup
+ signinForm!:FormGroup
+ textColor:string="red"
+ errorMsgFontSize:number=.7
   
-
+constructor(private formBuilder:FormBuilder){}
 ngOnInit() {
-  this.form=new FormGroup({
-     email:new FormControl('', [Validators.required]),
-     password:new FormControl('', [Validators.required])
+  this.signinForm=this.formBuilder.group({
+     email:['', [Validators.required,Validators.email]],
+     password:['', [Validators.required]]
   })
+}
+onSubmit(){
+  if(this.signinForm.valid){
+     console.log("sign in successful")
+  }
+  else{
+    console.log("invalid")
+  }
 }
 
 }
