e850836d70861eb92cdfbf9dfa3440e73465d387
Joshua Ondieki <oj@joshuaondieki.com>
2023-05-31 05:02:05 +0300

    signup, signin, flash messages, and intercept requests

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,34 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup,ReactiveFormsModule,FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
-  selector: 'app-signin',
-  standalone: true,
-  imports: [CommonModule,ReactiveFormsModule, RouterModule],
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+    selector: 'app-signin',
+    standalone: true,
+    imports: [CommonModule, ReactiveFormsModule, RouterModule],
+    templateUrl: './signin.component.html',
+    styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
- signinForm!:FormGroup
- textColor:string="red"
- errorMsgFontSize:number=.7
-  
-constructor(private formBuilder:FormBuilder){}
-ngOnInit() {
-  this.signinForm=this.formBuilder.group({
-     email:['', [Validators.required,Validators.email]],
-     password:['', [Validators.required]]
-  })
-}
-onSubmit(){
-  if(this.signinForm.valid){
-     console.log("sign in successful")
-  }
-  else{
-    console.log("invalid")
-  }
-}
+    signinForm!: FormGroup
+    textColor: string = "red"
+    errorMsgFontSize: number = .7
+
+    constructor(private formBuilder: FormBuilder, private userSvc:UserService) { }
+
+    ngOnInit() {
+        this.signinForm = this.formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required]]
+        })
+    }
+
+    onSubmit() {
+        if (this.signinForm.valid) {
+            this.userSvc.signin(this.signinForm.value)
+        }
+        else {
+            console.log("invalid")
+        }
+    }
 
 }
--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -9,3 +9,25 @@ export interface IProduct {
 export interface ICartItem extends IProduct {
     quantity:number
 }
+
+export interface IflashMessage {
+    type: 'error' | 'success' | 'info'
+    message:string
+}
+
+export interface IloggedUser {
+    email:string
+    token:string
+}
+
+export interface InewUserData {
+    firstname:string
+    lastname:string
+    email:string
+    password:string
+}
+
+export interface IuserCredentials {
+    email:string
+    password:string
+}
