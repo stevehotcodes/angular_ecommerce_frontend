8eccdf6dfcb1f6a362071cd72191816b409e9546
stevehotcodes <stevehotcodes@gmail.com>
2023-05-31 18:26:43 +0300

    created the display all products component

---
+++ src/app/components/display-products/display-products.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsService } from 'src/app/services/products.service';
+import { Iproduct } from 'src/app/interfaces';
+import { ProductCardComponent } from '../product-card/product-card.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-display-products',
+  standalone: true,
+  imports: [CommonModule,ProductCardComponent,RouterModule],
+  templateUrl: './display-products.component.html',
+  styleUrls: ['./display-products.component.css']
+})
+export class DisplayProductsComponent {
+    products:Iproduct[]=[]
+
+    constructor (private productService :ProductsService){ 
+    
+      this.productService.getAllProducts().subscribe(
+        res=>{
+          console.log(res)
+          this.products = res
+        }
+       
+        )
+    }
+  }
+    
+   
+
