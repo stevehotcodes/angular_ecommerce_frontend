583c76f3ead93e462383220d26fd891546d52e19
Joshua Ondieki <oj@joshuaondieki.com>
2023-06-02 00:54:59 +0300

    display items in the checkout page

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,31 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup, ReactiveFormsModule,FormControl } from '@angular/forms';
-import { IcartItem, Iproduct } from 'src/app/interfaces';
+import { FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { IcartItem } from 'src/app/interfaces';
+import { CartService } from 'src/app/services/cart.service';
 
 
 @Component({
-  selector: 'app-checkout',
-  standalone: true,
-  imports: [CommonModule,ReactiveFormsModule],
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+    selector: 'app-checkout',
+    standalone: true,
+    imports: [CommonModule, ReactiveFormsModule],
+    templateUrl: './checkout.component.html',
+    styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit {
- items:IcartItem[] = []
- total:number = this.items.reduce((accum:number, curr:IcartItem) => {
-  return accum + (curr.price * curr.quantity)
- }, 0)
+export class CheckoutComponent {
+    items!: IcartItem[]
+    total!: number
 
- checkoutForm!:FormGroup
+    constructor(private cartSvc: CartService) {
+        this.items = cartSvc.cartItems
+        this.total = cartSvc.cartTotal
+    }
 
- constructor(){}
-
- ngOnInit(): void {
-   this.checkoutForm=new FormGroup({
-      customerName: new FormControl(''),
-      town:new FormControl(''),
-      email:new FormControl('')
-   })
- }
 }
